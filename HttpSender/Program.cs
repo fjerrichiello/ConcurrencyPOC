e2175126f038613b532da5516b0111dde13e348e
Frank Errichiello <fjerrichiello@gmail.com>
2024-07-22 14:39:27 -0500

    Add http sender

---
+++ HttpSender/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+
+var httpClient = new HttpClient();
+
+
+// var task1 = httpClient.PostAsJsonAsync(new Uri("http://localhost:5197/add-book-request"),
+//     new AddBookRequest("Dr. Seuss", "1 Fish 2 Fish"));
+//
+// var task2 = httpClient.PostAsJsonAsync(new Uri("http://localhost:5197/add-book-request"),
+//     new AddBookRequest("Dr. Seuss", "Green Eggs and Ham"));
+//
+var task3 = httpClient.PostAsJsonAsync(new Uri("http://localhost:5197/add-book-request"),
+    new AddBookRequest("Dr. Seuss", "The Lorax"));
+
+var task4 = httpClient.PostAsJsonAsync(new Uri("http://localhost:5197/add-book-request"),
+    new AddBookRequest("Dr. Seuss", "Cat in the Hat"));
+
+var results = await Task.WhenAll(task3, task4);
+
+
+public record AddBookRequest(string AuthorId, string Title);
