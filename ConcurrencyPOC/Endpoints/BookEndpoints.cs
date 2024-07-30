43fa3b12cf19c6ca09d5a3a18d702b30f9ec2eab
Frank Errichiello <fjerrichiello@gmail.com>
2024-07-30 15:43:12 -0500

    Add test

--- ConcurrencyPOC/Endpoints/BookEndpoints.cs
+++ ConcurrencyPOC/Endpoints/BookEndpoints.cs
@@ -1,7 +1,12 @@
 ﻿using ConcurrencyPOC.DTOs;
+using ConcurrencyPOC.Enums;
 using ConcurrencyPOC.Handlers;
+using ConcurrencyPOC.Persistence;
+using ConcurrencyPOC.Persistence.Models;
+using ConcurrencyPOC.Persistence.Repositories;
 using Dumpify;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConcurrencyPOC.Endpoints;
 
@@ -10,7 +15,24 @@ public static class BookEndpoints
     public static void MapBookEndpoints(this WebApplication app)
     {
         app.MapPost("/add-book-request", AddBookRequest);
+        app.MapPost("/add-book-decline-reason-test", AddBookDeclineReason);
         app.MapPost("/add-book-request-two", AddBookRequestTwo);
+    }
+
+    private static async Task AddBookDeclineReason([FromServices] ApplicationDbContext dbContext)
+    {
+        var bookRequest = new BookRequest()
+        {
+            ApprovalStatus = ApprovalStatus.Pending,
+            AuthorId = "New Author",
+            DeclineReasons = [DeclineReason.Reason1, DeclineReason.Reason2],
+            MainId = Guid.NewGuid(),
+            RequestType = RequestType.Add,
+            Title = "Test"
+        };
+
+        await dbContext.AddAsync(bookRequest);
+        await dbContext.SaveChangesAsync();
     }
 
     // private static async Task AddBookRequest(HttpContext context,
